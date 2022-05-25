b97472c4c326e896945ac2c64bec3b1581ec8f65
Simon <simonprodhomme67@gmail.com>
2022-05-25 22:26:14 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterContainer } from './container/footer'
+
+
+
+function App() {
+  return (
+    <>
+      <FooterContainer/>
+    </>
+  );
+}
+
+export default App;
